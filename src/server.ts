312373d5f4f412c6d730ec1550c9d6e7c7d6f57e
GB-Navarro <gbnavarro@usp.br>
2022-09-28 20:58:25 -0300

    refactor: added a breakline on the server listening message

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT} \n`);
 })
