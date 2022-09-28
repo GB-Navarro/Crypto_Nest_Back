20577a4c10d5168179a81f8f7a4e9e7065923ed3
GB-Navarro <gbnavarro@usp.br>
2022-09-28 19:56:17 -0300

    fix: fixed a issue involving the server app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import router from "./routes/router";
 
 const app = express();
 
-app.use(cors);
-app.use(express.json);
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
 export default app;
