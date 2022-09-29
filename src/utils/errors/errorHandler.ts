be43983ba1ce93e60e92c9aeb7acdd1ca0ef28f3
GB-Navarro <gbnavarro@usp.br>
2022-09-29 09:12:58 -0300

    feat: added the passwordsAreNotEqual error to the errorHandler

--- src/utils/errors/errorHandler.ts
+++ src/utils/errors/errorHandler.ts
@@ -2,5 +2,8 @@ import { Request, Response, NextFunction } from "express";
 
 export default async function errorHandler(error: any, req: Request, res: Response, next: NextFunction){
 
+    if(error.type === "passwordsAreNotEqual"){
+        return res.send(error.message).status(404);
+    }
     return res.sendStatus(500);
 }
