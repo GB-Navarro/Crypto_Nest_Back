b8196b847463a8fe7fed2bd4951e4fc0c321edaa
GB-Navarro <gbnavarro@usp.br>
2022-09-30 10:32:30 -0300

    feat: exported the textSchemas

--- src/schemas/text/textSchemas.ts
+++ src/schemas/text/textSchemas.ts
@@ -2,9 +2,16 @@ import { createTextInterface } from "../../interfaces/textInterfaces/textInterfa
 
 import Joi from "joi";
 
-const createText = Joi.object<createTextInterface>({
+const create = Joi.object<createTextInterface>({
     type: Joi.string().required().allow(["news", "educational"]),
     tittle: Joi.string().min(5).required(),
     text: Joi.string().min(100).required(),
     category: Joi.string().min(4).required()
 })
+
+const textSchemas = {
+    create
+}
+
+export default textSchemas;
+
