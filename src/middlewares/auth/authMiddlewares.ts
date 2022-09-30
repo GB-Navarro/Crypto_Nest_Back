c0e171fcf4da5b804c8307971e176efff5f6e4a7
GB-Navarro <gbnavarro@usp.br>
2022-09-30 09:42:49 -0300

    feat: created the validateToken function

--- src/middlewares/auth/authMiddlewares.ts
+++ src/middlewares/auth/authMiddlewares.ts
@@ -1,12 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import { signUpInterface } from "../../interfaces/authInterfaces/authInterfaces";
+import authUtils from "../../utils/auth/authUtils";
 
-async function checkPasswordsEquality(req: Request, res: Response, next: NextFunction) {
+function checkPasswordsEquality(req: Request, res: Response, next: NextFunction) {
     const { password, confirmedPassword }: Omit<signUpInterface, "name" | "email"> = req.body;
 
     if (password != confirmedPassword) {
         throw ({ type: "passwordsAreNotEqual", message: "Passwords aren't equal!" });
     }
+
+    next();
+}
+
+function validateToken(req: Request, res: Response, next: NextFunction){
+    const token = req.headers.authorization;
+
+    if(token === undefined){
+        throw ({ type: "invalidToken", message: "Invalid token!" });
+    }
+
+    const data = authUtils.getTokenDataOrFail(token);
+
+    res.locals.data = data;
 
     next();
 }
