0e76cdda043113e3dbaf1f17f9253200606416ef
GB-Navarro <gbnavarro@usp.br>
2022-09-30 09:43:08 -0300

    feat: exported the validateToken function

--- src/middlewares/auth/authMiddlewares.ts
+++ src/middlewares/auth/authMiddlewares.ts
@@ -27,7 +27,8 @@ function validateToken(req: Request, res: Response, next: NextFunction){
 }
 
 const authMiddlewares = {
-    checkPasswordsEquality
+    checkPasswordsEquality,
+    validateToken
 }
 
 export default authMiddlewares;
