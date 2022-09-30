4bac7ede272dacba781d4f7aca0349d4a3682844
GB-Navarro <gbnavarro@usp.br>
2022-09-30 10:40:16 -0300

    fix: removed the type attribute of the create schema

--- src/schemas/text/textSchemas.ts
+++ src/schemas/text/textSchemas.ts
@@ -3,7 +3,6 @@ import { createTextInterface } from "../../interfaces/textInterfaces/textInterfa
 import Joi from "joi";
 
 const create = Joi.object<createTextInterface>({
-    type: Joi.string().required().allow(["news", "educational"]),
     tittle: Joi.string().min(5).required(),
     text: Joi.string().min(100).required(),
     category: Joi.string().min(4).required()
