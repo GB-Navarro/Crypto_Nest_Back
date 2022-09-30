ef9d98ef758e51caee8e415cc86f3ca74c0cdc6e
GB-Navarro <gbnavarro@usp.br>
2022-09-29 21:38:50 -0300

    fix: fixed a issue involving the return of errorHandler

--- src/utils/errors/errorHandler.ts
+++ src/utils/errors/errorHandler.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 export default async function errorHandler(error: any, req: Request, res: Response, next: NextFunction){
 
     if(error.type === "passwordsAreNotEqual"){
-        return res.send(error.message).status(404);
+        return res.status(404).send(error.message);
     }
     return res.sendStatus(500);
 }
