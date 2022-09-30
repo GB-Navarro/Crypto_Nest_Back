4acee590b60f3e9d76590775c6bf8e7d0ae8fe74
GB-Navarro <gbnavarro@usp.br>
2022-09-30 08:23:43 -0300

    feat: added the encryptPassword function

--- src/utils/auth/authUtils.ts
+++ src/utils/auth/authUtils.ts
@@ -1,16 +1,24 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import bcrypt from "bcrypt";
 
 dotenv.config();
 
-function generateToken(userEmail: string) {
+function encryptPassword(password: string){
 
+    const encryptPassword = bcrypt.hashSync(password,10);
+    
+    return encryptPassword;
+}
+
+function generateToken(userEmail: string) {
     const token = jwt.sign(userEmail, process.env.JWT_KEY,);
 
     return token;
 }
 
 const authUtils = {
+    encryptPassword,
     generateToken
 }
 
