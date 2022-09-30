25afad9708bc30acbd18d2f0233b1bca985c9281
GB-Navarro <gbnavarro@usp.br>
2022-09-30 07:34:03 -0300

    feat: created the signIn schema

--- src/schemas/auth/authSchemas.ts
+++ src/schemas/auth/authSchemas.ts
@@ -1,4 +1,4 @@
-import { signUpInterface } from "../../interfaces/authInterfaces/authInterfaces";
+import { signUpInterface, signInInterface } from "../../interfaces/authInterfaces/authInterfaces";
 
 import Joi from "joi";
 
@@ -9,8 +9,15 @@ const signUp = Joi.object<signUpInterface>({
     confirmedPassword: Joi.string().min(8).max(20).required()
 })
 
+const signIn = Joi.object<signInInterface>({
+    name: Joi.string().min(4).max(15).required(),
+    email: Joi.string().email({ tlds: { allow: false } }).min(7).max(40).required(),
+    password: Joi.string().min(8).max(20).required()
+})
+
 const authSchemas = {
-    signUp
+    signUp,
+    signIn
 }
 
 export default authSchemas;
