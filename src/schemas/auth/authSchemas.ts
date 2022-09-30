f062f3acc61fe29f5b65c0c7487c4c53874b1cd1
GB-Navarro <gbnavarro@usp.br>
2022-09-29 21:39:25 -0300

    feat: updated some signUp schema attributes

--- src/schemas/auth/authSchemas.ts
+++ src/schemas/auth/authSchemas.ts
@@ -4,9 +4,9 @@ import Joi from "joi";
 
 const signUp = Joi.object<signUpInterface>({
     name: Joi.string().min(4).max(15).required(),
-    email: Joi.string().email({ tlds: { allow: false } }).min(7).max(20).required(),
-    password: Joi.string().min(8).max(10).required(),
-    confirmedPassword: Joi.string().min(8).max(10).required()
+    email: Joi.string().email({ tlds: { allow: false } }).min(7).max(40).required(),
+    password: Joi.string().min(8).max(20).required(),
+    confirmedPassword: Joi.string().min(8).max(20).required()
 })
 
 const authSchemas = {
