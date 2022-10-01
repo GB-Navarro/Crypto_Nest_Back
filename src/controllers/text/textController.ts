c8be07a65ca8f9a768ed68076aa774c139714780
GB-Navarro <gbnavarro@usp.br>
2022-10-01 09:02:19 -0300

    feat: created the create function

--- src/controllers/text/textController.ts
+++ src/controllers/text/textController.ts
@@ -1,8 +1,20 @@
 import { Request, Response } from "express";
 import { createTextInterface } from "../../interfaces/textInterfaces/textInterfaces";
 
-async function create(req: Request, res: Response){
+import articleServices from "../../services/articles/articleServices";
+
+async function create(req: Request, res: Response) {
+
+    const type = req.params.type;
+    const userInfo = res.locals.data;
+
     const data: createTextInterface = req.body;
+
+    if (type === "news") {
+
+    } else {
+        await articleServices.create(data, userInfo);
+    }
 
     res.send("Created").status(201);
 }
