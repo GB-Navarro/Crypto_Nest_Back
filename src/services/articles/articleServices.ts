60cb7a99d82f8f2dcddb269a7a3078ca65e75398
GB-Navarro <gbnavarro@usp.br>
2022-10-01 10:03:11 -0300

    style: changed some codes positions

--- src/services/articles/articleServices.ts
+++ src/services/articles/articleServices.ts
@@ -1,9 +1,28 @@
-import { createTextInterface } from "../../interfaces/textInterfaces/textInterfaces";
-import { userInfoInterface } from "../../interfaces/userInterfaces/userInterfaces";
 import { articles, articlesCategory, userArticles } from "@prisma/client";
+import { userInfoInterface } from "../../interfaces/userInterfaces/userInterfaces";
+import { createTextInterface } from "../../interfaces/textInterfaces/textInterfaces";
 
-import articleRepository from "../../repositories/articles/articleRepository";
 import articleUtils from "../../utils/article/articleUtils";
+import articleRepository from "../../repositories/articles/articleRepository";
+
+async function checkTittleExistence(tittle: string) {
+
+    const tittleExist: articles = await articleRepository.getByTittle(tittle);
+
+    if (tittleExist) {
+        throw ({ type: "tittleAlreadyExist", message: "This tittle already exist!" });
+    }
+}
+async function getCategoryIdByName(categoryName: string) {
+
+    const categoryId = await articleRepository.getCategoryIdByName(categoryName);
+
+    if (categoryId === null) {
+        throw ({ type: "categoryDoNotExist", message: "This category do not exist!" });
+    }
+
+    return categoryId;
+}
 
 async function create(data: createTextInterface, userInfo: userInfoInterface) {
 
@@ -21,26 +40,6 @@ async function create(data: createTextInterface, userInfo: userInfoInterface) {
     const relationshipData: Omit<userArticles, "id"> = articleUtils.generateRelationshipData(userId, articleId);
 
     await articleRepository.createRelationship(relationshipData);
-}
-
-async function getCategoryIdByName(categoryName: string) {
-
-    const categoryId = await articleRepository.getCategoryIdByName(categoryName);
-
-    if (categoryId === null) {
-        throw ({ type: "categoryDoNotExist", message: "This category do not exist!" });
-    }
-
-    return categoryId;
-}
-
-async function checkTittleExistence(tittle: string) {
-
-    const tittleExist: articles = await articleRepository.getByTittle(tittle);
-
-    if (tittleExist) {
-        throw ({ type: "tittleAlreadyExist", message: "This tittle already exist!" });
-    }
 }
 
 const articleServices = {
