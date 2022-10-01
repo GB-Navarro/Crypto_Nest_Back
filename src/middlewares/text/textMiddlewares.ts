48a3e15dd8fcbedd0442eede9af1b574fb742b79
GB-Navarro <gbnavarro@usp.br>
2022-10-01 10:29:35 -0300

    fix: fixed a issue involving the param validation

--- src/middlewares/text/textMiddlewares.ts
+++ src/middlewares/text/textMiddlewares.ts
@@ -2,10 +2,12 @@ import { Request, Response, NextFunction, text } from "express";
 
 function validateUrlParam(req: Request, res: Response, next: NextFunction) {
 
-    const validParams = ["news, educational"];
+    const validParams = ["news", "educational"];
     const receivedParam = req.params.type;
 
-    if(!(receivedParam in validParams)){
+    const isValid = validParams.find((element) => element == receivedParam);
+
+    if(!(isValid)){
         throw ({ type: "invalidParam", message: "Invalid text type!" });
     }
 
