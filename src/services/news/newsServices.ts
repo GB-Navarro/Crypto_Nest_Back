e6fbf5cf75dea31bb0a90b17c32e747225987d09
GB-Navarro <gbnavarro@usp.br>
2022-10-01 10:01:53 -0300

    feat: created the create function

--- src/services/news/newsServices.ts
+++ src/services/news/newsServices.ts
@@ -1,5 +1,8 @@
-import { news, newsCategory } from "@prisma/client";
+import { news, newsCategory, userNews } from "@prisma/client";
+import { userInfoInterface } from "../../interfaces/userInterfaces/userInterfaces";
+import { createTextInterface } from "../../interfaces/textInterfaces/textInterfaces";
 
+import newsUtils from "../../utils/news/newsUtils";
 import newsRepository from "../../repositories/news/newsRepository";
 
 async function checkTittleExistence(tittle: string) {
@@ -22,8 +25,26 @@ async function getCategoryIdByName(categoryName: string) {
     return categoryId;
 }
 
-const newsServices = {
+async function create(data: createTextInterface, userInfo: userInfoInterface) {
 
+    const { tittle, text, category: categoryName }: { tittle: string, text: string, category: string } = data;
+    const { userId }: { userId: number } = userInfo;
+
+    await checkTittleExistence(tittle);
+
+    const { id: categoryId }: Partial<newsCategory> = await getCategoryIdByName(categoryName);
+
+    const news: Omit<news, "id" | "date"> = newsUtils.generateNewsData(tittle, text, categoryId);
+
+    const { id: newsId }: Partial<news> = await newsRepository.createAndReturn(news);
+
+    const relationshipData: Omit<userNews, "id"> = newsUtils.generateRelationshipData(userId, newsId);
+
+    await newsRepository.createRelationship(relationshipData);
+}
+
+const newsServices = {
+    create
 }
 
 export default newsServices;
