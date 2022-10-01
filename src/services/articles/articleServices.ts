8a3fbcaf15e13abc594040da13ac50e0589132ec
GB-Navarro <gbnavarro@usp.br>
2022-10-01 08:57:05 -0300

    feat: created the checkTittleExistence function

--- src/services/articles/articleServices.ts
+++ src/services/articles/articleServices.ts
@@ -1,18 +1,29 @@
+import { articles } from "@prisma/client";
+
 import articleRepository from "../../repositories/articles/articleRepository";
 
 async function getCategoryIdByName(categoryName: string) {
 
     const categoryId = await articleRepository.getCategoryIdByName(categoryName);
 
-    if(categoryId === null){
+    if (categoryId === null) {
         throw ({ type: "categoryDoNotExist", message: "This category do not exist!" });
     }
 
     return categoryId;
 }
 
+async function checkTittleExistence(tittle: string){
+
+    const tittleExist: articles = await articleRepository.getByTittle(tittle);
+
+    if (tittleExist) {
+        throw ({ type: "tittleAlreadyExist", message: "This tittle already exist!" });
+    }
+}
+
 const articleServices = {
-    getCategoryIdByName
+    
 }
 
 export default articleServices;
