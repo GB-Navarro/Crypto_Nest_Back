1c601697db2c97c76cd6dd8dc45c6a254bff13e5
GB-Navarro <gbnavarro@usp.br>
2022-10-10 07:48:30 -0300

    fix: removed some unecessary info

--- src/controllers/coins/coinsController.ts
+++ src/controllers/coins/coinsController.ts
@@ -1,48 +1,21 @@
 import { Request, Response } from "express";
-import { UserInfo } from "../../interfaces/userInterfaces/userInterfaces";
 
 import coinsServices from "../../services/coins/coinsServices";
 
 async function getAll(req: Request, res: Response) {
 
-    const { userId, userName, userEmail } = res.locals.data;
-
-    const userInfo: UserInfo = {
-        id: userId,
-        name: userName,
-        email: userEmail
-    }
-
     const data = await coinsServices.getAll();
 
-    const response = {
-        userInfo,
-        data
-    }
-
-    res.status(200).send(response);
+    res.status(200).send(data);
 }
 
 async function getById(req: Request, res: Response) {
-
-    const { userId, userEmail, userName } = res.locals.data;
-
-    const userInfo: UserInfo = {
-        id: userId,
-        name: userName,
-        email: userEmail
-    }
 
     const { id: coinId } = req.params;
 
     const data = await coinsServices.getById(coinId);
 
-    const response = {
-        userInfo,
-        data
-    }
-
-    res.status(200).send(response);
+    res.status(200).send(data);
 }
 
 const coinsController = {
