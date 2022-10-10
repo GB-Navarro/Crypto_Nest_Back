5adc8421a3de3a126cb3558093315667293ce31b
GB-Navarro <gbnavarro@usp.br>
2022-10-10 07:05:38 -0300

    feat: added the userInfo in the response

--- src/controllers/coins/coinsController.ts
+++ src/controllers/coins/coinsController.ts
@@ -1,19 +1,48 @@
 import { Request, Response } from "express";
+import { UserInfo } from "../../interfaces/userInterfaces/userInterfaces";
 
 import coinsServices from "../../services/coins/coinsServices";
 
 async function getAll(req: Request, res: Response) {
+
+    const { userId, userName, userEmail } = res.locals.data;
+
+    const userInfo: UserInfo = {
+        id: userId,
+        name: userName,
+        email: userEmail
+    }
+
     const data = await coinsServices.getAll();
 
-    res.status(200).send(data);
+    const response = {
+        userInfo,
+        data
+    }
+
+    res.status(200).send(response);
 }
 
 async function getById(req: Request, res: Response) {
+
+    const { userId, userEmail, userName } = res.locals.data;
+
+    const userInfo: UserInfo = {
+        id: userId,
+        name: userName,
+        email: userEmail
+    }
+
     const { id: coinId } = req.params;
 
     const data = await coinsServices.getById(coinId);
 
-    res.status(200).send(data);
+    const response = {
+        userInfo,
+        data
+    }
+
+    res.status(200).send(response);
 }
 
 const coinsController = {
